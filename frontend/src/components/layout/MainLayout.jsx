3ca9aae9154b249d2700f81649b4527e5a0dcd83
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-07-05 12:55:03 +0530

    add search bar component

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "../search/SearchBar";
 
 export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col bg-slate-100">
 
       <Navbar />
+
+      <SearchBar />
 
       <main className="flex-1">
         {children}
