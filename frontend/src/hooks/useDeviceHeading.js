2e7d8ec6b0be196d259d65465a0575d396eac192
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-09-13 15:38:59 +0530

    Update useDeviceHeading.js

--- frontend/src/hooks/useDeviceHeading.js
+++ frontend/src/hooks/useDeviceHeading.js
@@ -33,7 +33,7 @@ export default function useDeviceHeading() {
       // Android / other browsers
       // ------------------------------------------
       else if (typeof event.alpha === "number") {
-        nextHeading = 360 - event.alpha;
+        nextHeading = 360 - event.alpha + 90;
       }
 
       if (nextHeading === null) {
